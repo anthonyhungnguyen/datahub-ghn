f548acb62d9f9f0490415094baa51e8b106af550
hungnp <hungnp1@ghn.vn>
2024-05-02 21:00:50 -0400

    initial commit

---
+++ datahub-frontend/app/utils/ConfigUtil.java
@@ -0,0 +1,71 @@
+package utils;
+
+import com.linkedin.util.Configuration;
+import com.typesafe.config.Config;
+
+public class ConfigUtil {
+
+  private ConfigUtil() {}
+
+  // New configurations, provided via application.conf file.
+  public static final String METADATA_SERVICE_HOST_CONFIG_PATH = "metadataService.host";
+  public static final String METADATA_SERVICE_PORT_CONFIG_PATH = "metadataService.port";
+  public static final String METADATA_SERVICE_USE_SSL_CONFIG_PATH = "metadataService.useSsl";
+  public static final String METADATA_SERVICE_SSL_PROTOCOL_CONFIG_PATH =
+      "metadataService.sslProtocol";
+
+  // GHN SSO
+  public static final String GHN_SSO_LOGIN_URL = "GHN_SSO_LOGIN_URL";
+  public static final String GHN_SSO_REDIRECT_URL = "GHN_SSO_REDIRECT_URL";
+  public static final String GHN_SSO_GEN_ACCESS_TOKEN_URL = "GHN_SSO_GEN_ACCESS_TOKEN_URL";
+  public static final String GHN_SSO_VERIFY_ACCESS_TOKEN_URL = "GHN_SSO_VERIFY_ACCESS_TOKEN_URL";
+  public static final String GHN_SSO_APP_KEY = "GHN_SSO_APP_KEY";
+  public static final String GHN_SSO_APP_SECRET = "GHN_SSO_APP_SECRET";
+  public static final String GHN_EMPLOYEE_INFO_URL = "GHN_EMPLOYEE_INFO_URL";
+  public static final String GHN_EMPLOYEE_INFO_AUTH = "GHN_EMPLOYEE_INFO_AUTH";
+
+  // Legacy env-var based config values, for backwards compatibility:
+  public static final String GMS_HOST_ENV_VAR = "DATAHUB_GMS_HOST";
+  public static final String GMS_PORT_ENV_VAR = "DATAHUB_GMS_PORT";
+  public static final String GMS_USE_SSL_ENV_VAR = "DATAHUB_GMS_USE_SSL";
+  public static final String GMS_SSL_PROTOCOL_VAR = "DATAHUB_GMS_SSL_PROTOCOL";
+
+  // Default values
+  public static final String DEFAULT_GMS_HOST = "localhost";
+  public static final String DEFAULT_GMS_PORT = "8080";
+  public static final String DEFAULT_GMS_USE_SSL = "False";
+
+  public static final String DEFAULT_METADATA_SERVICE_HOST =
+      Configuration.getEnvironmentVariable(GMS_HOST_ENV_VAR, "localhost");
+  public static final Integer DEFAULT_METADATA_SERVICE_PORT =
+      Integer.parseInt(Configuration.getEnvironmentVariable(GMS_PORT_ENV_VAR, "8080"));
+  public static final Boolean DEFAULT_METADATA_SERVICE_USE_SSL =
+      Boolean.parseBoolean(Configuration.getEnvironmentVariable(GMS_USE_SSL_ENV_VAR, "False"));
+  public static final String DEFAULT_METADATA_SERVICE_SSL_PROTOCOL =
+      Configuration.getEnvironmentVariable(GMS_SSL_PROTOCOL_VAR);
+
+  public static final String DEFAULT_GHN_SSO_LOGIN_URL = Configuration.getEnvironmentVariable(GHN_SSO_LOGIN_URL, "https://sso-v2.ghn.vn/internal/login");
+  public static final String DEFAULT_GHN_SSO_REDIRECT_URL = Configuration.getEnvironmentVariable(GHN_SSO_REDIRECT_URL, "http://localhost:3000");
+  public static final String DEFAULT_GHN_SSO_GEN_ACCESS_TOKEN_URL = Configuration.getEnvironmentVariable(GHN_SSO_GEN_ACCESS_TOKEN_URL, "https://online-gateway.ghn.vn/sso-v2/public-api/staff/gen-access-token");
+  public static final String DEFAULT_GHN_SSO_VERIFY_ACCESS_TOKEN_URL = Configuration.getEnvironmentVariable(GHN_SSO_VERIFY_ACCESS_TOKEN_URL, "https://online-gateway.ghn.vn/sso-v2/public-api/staff/verify-access-token");
+  public static final String DEFAULT_GHN_SSO_APP_KEY = Configuration.getEnvironmentVariable(GHN_SSO_APP_KEY, "62fb5476-3ed5-4431-9250-0aaa0bdda05f");
+  public static final String DEFAULT_GHN_SSO_APP_SECRET = Configuration.getEnvironmentVariable(GHN_SSO_APP_SECRET, "f19f0e2e-b03b-4bf2-9ac2-10dd65f7355d");
+  public static final String DEFAULT_GHN_EMPLOYEE_INFO_URL = Configuration.getEnvironmentVariable(GHN_EMPLOYEE_INFO_URL, "https://online-gateway.ghn.vn/hrm/core/api/employee/detail");
+  public static final String DEFAULT_GHN_EMPLOYEE_INFO_AUTHORIZATION = Configuration.getEnvironmentVariable(GHN_EMPLOYEE_INFO_AUTH, "Basic b25saW5lOkk1U3tnfn51Mjs=");
+
+  public static boolean getBoolean(Config config, String key) {
+    return config.hasPath(key) && config.getBoolean(key);
+  }
+
+  public static boolean getBoolean(Config config, String key, boolean defaultValue) {
+    return config.hasPath(key) ? config.getBoolean(key) : defaultValue;
+  }
+
+  public static int getInt(Config config, String key, int defaultValue) {
+    return config.hasPath(key) ? config.getInt(key) : defaultValue;
+  }
+
+  public static String getString(Config config, String key, String defaultValue) {
+    return config.hasPath(key) ? config.getString(key) : defaultValue;
+  }
+}
---
+++ datahub-web-react/src/app/auth/LogIn.tsx
@@ -0,0 +1,233 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import * as QueryString from 'query-string';
+import { Input, Button, Form, message, Image, Divider } from 'antd';
+import { UserOutlined, LockOutlined, LoginOutlined } from '@ant-design/icons';
+import { useReactiveVar } from '@apollo/client';
+import styled, { useTheme } from 'styled-components/macro';
+import { Redirect, useLocation } from 'react-router';
+import styles from './login.module.css';
+import { Message } from '../shared/Message';
+import { isLoggedInVar, checkAuthStatus } from './checkAuthStatus';
+import analytics, { EventType } from '../analytics';
+import { useAppConfig } from '../useAppConfig';
+
+type FormValues = {
+    username: string;
+    password: string;
+};
+
+const FormInput = styled(Input)`
+    &&& {
+        height: 32px;
+        font-size: 12px;
+        border: 1px solid #555555;
+        border-radius: 5px;
+        background-color: transparent;
+        color: white;
+        line-height: 1.5715;
+    }
+    > .ant-input {
+        color: white;
+        font-size: 14px;
+        background-color: transparent;
+    }
+    > .ant-input:hover {
+        color: white;
+        font-size: 14px;
+        background-color: transparent;
+    }
+`;
+
+const SsoDivider = styled(Divider)`
+    background-color: white;
+`;
+
+const SsoButton = styled(Button)`
+    &&& {
+        align-self: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 5.6px 11px;
+        gap: 4px;
+    }
+`;
+
+const LoginLogo = styled(LoginOutlined)`
+    padding-top: 7px;
+`;
+
+const SsoTextSpan = styled.span`
+    padding-top: 6px;
+`;
+
+const redirectSSO = async () => {
+    const requestOptions = {
+        method: 'GET',
+    };
+    await fetch('/getRedirectUri', requestOptions).then(async (response) => {
+        const data = await response.text();
+        if (!response.ok) {
+            const error = data || response.status;
+            return Promise.reject(error);
+        }
+        window.location.href = data;
+        return Promise.resolve();
+    });
+};
+
+export type LogInProps = Record<string, never>;
+
+export const LogIn: React.VFC<LogInProps> = () => {
+    const isLoggedIn = useReactiveVar(isLoggedInVar);
+    const location = useLocation();
+    const params = QueryString.parse(location.search);
+    const maybeRedirectError = params.error_msg;
+
+    const themeConfig = useTheme();
+    const [loading, setLoading] = useState(false);
+
+    const { refreshContext } = useAppConfig();
+
+    const redirectUri = params.redirect_uri;
+    useEffect(() => {
+        if (redirectUri) {
+            const code = redirectUri.toString().split('=')[1];
+            if (code) {
+                setLoading(true);
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ authorization_code: code }),
+                };
+
+                fetch('/genAccessToken', requestOptions)
+                    .then(async (response) => {
+                        const data = await response.json();
+                        if (!response.ok) {
+                            const error = (data && data.message) || response.status;
+                            return Promise.reject(error);
+                        }
+                        const ipResponse = await fetch('https://api.ipify.org?format=json', { method: 'GET' });
+                        const ipData = await ipResponse.json();
+                        const remoteIp = ipData.ip;
+                        const verifyRequestOptions = {
+                            method: 'POST',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify({ ...data, remote_ip: remoteIp }),
+                        };
+                        return fetch('/verifyAccessToken', verifyRequestOptions);
+                    })
+                    .then(async (verifyResponse) => {
+                        if (!verifyResponse.ok) {
+                            const error = verifyResponse.status;
+                            return Promise.reject(error);
+                        }
+                        isLoggedInVar(true);
+                        refreshContext();
+                        analytics.event({ type: EventType.LogInEvent });
+                        return Promise.resolve();
+                    })
+                    .catch((e) => {
+                        console.error('Failed to log in! An unexpected error occurred.', e);
+                    })
+                    .finally(() => setLoading(false));
+            }
+        }
+    }, [redirectUri, refreshContext]);
+
+    const handleLogin = useCallback(
+        (values: FormValues) => {
+            setLoading(true);
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: values.username, password: values.password }),
+            };
+            fetch('/logIn', requestOptions)
+                .then(async (response) => {
+                    if (!response.ok) {
+                        const data = await response.json();
+                        const error = (data && data.message) || response.status;
+                        return Promise.reject(error);
+                    }
+                    isLoggedInVar(true);
+                    refreshContext();
+                    analytics.event({ type: EventType.LogInEvent });
+                    return Promise.resolve();
+                })
+                .catch((_) => {
+                    message.error(`Failed to log in! An unexpected error occurred.`);
+                })
+                .finally(() => setLoading(false));
+        },
+        [refreshContext],
+    );
+
+    if (isLoggedIn) {
+        const maybeRedirectUri = params.redirect_uri;
+        console.log('maybeRedirectUri', maybeRedirectUri);
+        return <Redirect to={(maybeRedirectUri && decodeURIComponent(maybeRedirectUri as string)) || '/'} />;
+    }
+
+    return (
+        <div className={styles.login_page}>
+            {maybeRedirectError && maybeRedirectError.length > 0 && (
+                <Message type="error" content={maybeRedirectError} />
+            )}
+            <div className={styles.login_box}>
+                <div className={styles.login_logo_box}>
+                    <Image wrapperClassName={styles.logo_image} src={themeConfig.assets?.logoUrl} preview={false} />
+                </div>
+                <div className={styles.login_form_box}>
+                    {loading && <Message type="loading" content="Logging in..." />}
+                    {/* <Form onFinish={handleLogin} layout="vertical">
+                        <Form.Item
+                            name="username"
+                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
+                            label={<label style={{ color: 'white' }}>Username</label>}
+                        >
+                            <FormInput prefix={<UserOutlined />} data-testid="username" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            // eslint-disable-next-line jsx-a11y/label-has-associated-control
+                            label={<label style={{ color: 'white' }}>Password</label>}
+                        >
+                            <FormInput prefix={<LockOutlined />} type="password" data-testid="password" />
+                        </Form.Item>
+                        <Form.Item style={{ marginBottom: '0px' }} shouldUpdate>
+                            {({ getFieldsValue }) => {
+                                const { username, password } = getFieldsValue();
+                                const formIsComplete = !!username && !!password;
+                                return (
+                                    <Button
+                                        type="primary"
+                                        block
+                                        htmlType="submit"
+                                        className={styles.login_button}
+                                        disabled={!formIsComplete}
+                                    >
+                                        Sign In
+                                    </Button>
+                                );
+                            }}
+                        </Form.Item>
+                    </Form> */}
+                    {/* <SsoDivider /> */}
+                    <SsoButton
+                        type="primary"
+                        onClick={redirectSSO}
+                        block
+                        htmlType="submit"
+                        className={styles.sso_button}
+                    >
+                        <LoginLogo />
+                        <SsoTextSpan>Sign in with GHN</SsoTextSpan>
+                        <span />
+                    </SsoButton>
+                </div>
+            </div>
+        </div>
+    );
+};
